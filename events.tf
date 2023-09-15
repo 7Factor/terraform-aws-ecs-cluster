2a74b95bba40864f4eb145f2d7779312772928da
Jeremy Wood <jeremy.wood@7factor.io>
2023-09-15 14:14:53 -0400

    Add preceding slash to log group name.

--- events.tf
+++ events.tf
@@ -28,7 +28,7 @@ resource "aws_cloudwatch_event_rule" "ecs_task_stopped" {
 }
 
 resource "aws_cloudwatch_log_group" "ecs_stopped_tasks" {
-  name = "aws/events/${var.stopped_ecs_task_log_group}"
+  name = "/aws/events/${var.stopped_ecs_task_log_group}"
 }
 
 resource "aws_cloudwatch_event_target" "log_ecs_task_stopped" {
